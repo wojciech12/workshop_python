467043615a6f245d361aaf58e16436b69d6c42c8
Bartosz Tyrala <automationdream@gmail.com>
2020-06-03 16:56:31 +0200

    2. Podstawowe operacje matematyczne, operatory, listy

--- 01_podstawy/01_Podstawy_1/podstawy_1.py
+++ 01_podstawy/01_Podstawy_1/podstawy_1.py
@@ -43,9 +43,6 @@ print(f"Hello {imie}")
 # """Mój kot ma na imię "Cicik" .. I'm happy....
 # Zacznijmy nowe zdanie..."""
 
-test = 1 + ".Hello"
-print(test)
-# powyższe polecenie wywoła błąd, czy wiesz dlaczego?
 
 # * List -> ['Anna','Barbara','Celina']
 # * Tuple -> ('Anna','Barbara','Celina')
@@ -63,4 +60,102 @@ print(test)
 
 # 2. Podstawowe operacje matematyczne
 
-# TBD...
+dodawanie = 1 + 3  # = 4
+print(dodawanie)
+
+odejmowanie = 1 - 3  # = -2
+print(odejmowanie)
+
+mnozenie = 1 * 3  # = 3
+print(mnozenie)
+
+dzielenie = 1 / 3  # = 0.33333333
+print(dzielenie)
+
+zaokraglenie = round(1.0 / 3, 2)  # = 0.33
+print(zaokraglenie)
+
+modulo = 7 % 3  # = 1
+print(modulo)
+
+# Kolejnosc wykonywania operacji - jak w matematyce
+
+print(2 + 3 * 2 - 1) # 3 * 2 -> 2 + 6 -> 8 - 1 -> 7
+
+# Kolejnosc wykonywania operacji cd..
+
+print((2 + 3) * (2 - 1)) # 2 + 3 -> 5 .. 2 - 1 -> 1 .. 5 * 1 -> 5
+
+# Laczenie cyfr i znakow
+
+# powyższe polecenie wywoła błąd, czy wiesz dlaczego?
+
+# test = 1 + ".Hello"
+# print(test)
+
+# W celu połączenia numerów i znaków powinniśmy je zadeklarować jako ten sam typ zmiennej:
+
+print(str(1) + ".Hello")
+
+# Operatory
+
+# =
+a = 1
+print(a)
+
+# ==
+a = 1
+b = 3
+
+test = (a == b) # False
+print(test)
+
+# a > b, a >= b, b <= a, b != a
+
+# operatory inkrementacji <- zwiekszenia
+
+# Rozwazmy przyklad:
+a = 1
+a = a + 1
+
+print(a)
+
+a = 1
+a += 1
+print(a)
+
+# -=
+# *=
+# /=
+
+# assert <- zalozenie, ze
+
+# assert 0 == 1  # Error
+assert 1 == 1  # no error
+
+# 3. Listy - modyfikowalne zbiory danych
+
+lista = ['Ania','Basia','Celina', 'Dorota']
+
+assert (lista[0] == 'Ania')  # True
+
+# Slice
+print(lista[:4])  # prints from beginning to end index
+print(lista[2:])  # prints from start index to end of list
+print(lista[2:4]) # prints from start index to end index
+print(lista[:])   # prints from beginning to end of list
+
+assert test == 'Dorota'
+
+lista.append('Ewelina')  # ['Ania', 'Basia', 'Celina', 'Dorota', 'Ewelina']
+lista.remove('Basia')  # ['Ania', 'Celina', 'Dorota', 'Ewelina']
+
+print(lista)
+
+assert lista.index("Celina") == 1
+
+# Modyfikowanie listy
+lista[1] = "Gosia"
+
+print(len(lista))  # ilosc elementow w liscie
+
